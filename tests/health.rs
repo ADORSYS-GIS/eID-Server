90796052f3c9084851e8b53952f1f3c19a3683f2
Hermann Core <hermannoenpause@gmail.com>
2025-04-24 10:12:42 +0100

    tests: setup initial integration tests

---
+++ tests/health.rs
@@ -0,0 +1,15 @@
+mod common;
+
+use reqwest::Client;
+
+#[tokio::test]
+async fn test_health_check_works() {
+    let addr = common::spawn_server().await;
+
+    let client = Client::new();
+    let response = client.get(format!("{}/health", addr)).send().await.unwrap();
+
+    // Verify the response
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
