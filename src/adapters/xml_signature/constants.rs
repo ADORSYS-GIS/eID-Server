6ea638745e44d2c043e498507dc903a2800df14c
Ngha-Boris <borisngha6@gmail.com>
2025-07-30 16:06:12 +0100

    chore: cargo machete

--- src/adapters/xml_signature/constants.rs
+++ src/adapters/xml_signature/constants.rs
@@ -36,41 +36,6 @@ pub const PEM_DSA_PRIVATE_KEY_TAG: &str = "DSA PRIVATE KEY";
 pub const PEM_EC_PRIVATE_KEY_TAG: &str = "EC PRIVATE KEY";
 pub const PEM_ENCRYPTED_PRIVATE_KEY_TAG: &str = "ENCRYPTED PRIVATE KEY";
 
-/// PEM tags for public keys
-pub const PEM_PUBLIC_KEY_TAG: &str = "PUBLIC KEY";
-pub const PEM_RSA_PUBLIC_KEY_TAG: &str = "RSA PUBLIC KEY";
-pub const PEM_DSA_PUBLIC_KEY_TAG: &str = "DSA PUBLIC KEY";
-pub const PEM_EC_PUBLIC_KEY_TAG: &str = "EC PUBLIC KEY";
-
-/// PEM tags for certificate requests
-pub const PEM_CERTIFICATE_REQUEST_TAG: &str = "CERTIFICATE REQUEST";
-pub const PEM_NEW_CERTIFICATE_REQUEST_TAG: &str = "NEW CERTIFICATE REQUEST";
-
-/// PEM tags for certificate revocation lists
-pub const PEM_X509_CRL_TAG: &str = "X509 CRL";
-pub const PEM_CRL_TAG: &str = "CRL";
-
-/// PEM tags for Diffie-Hellman parameters
-pub const PEM_DH_PARAMETERS_TAG: &str = "DH PARAMETERS";
-pub const PEM_X9_42_DH_PARAMETERS_TAG: &str = "X9.42 DH PARAMETERS";
-
-/// PEM tags for DSA parameters
-pub const PEM_DSA_PARAMETERS_TAG: &str = "DSA PARAMETERS";
-
-/// PEM tags for elliptic curve parameters
-pub const PEM_EC_PARAMETERS_TAG: &str = "EC PARAMETERS";
-
-/// PEM tags for PKCS#7 structures
-pub const PEM_PKCS7_TAG: &str = "PKCS7";
-pub const PEM_PKCS7_SIGNED_DATA_TAG: &str = "PKCS7 SIGNED DATA";
-
-/// PEM tags for SSH keys
-pub const PEM_OPENSSH_PRIVATE_KEY_TAG: &str = "OPENSSH PRIVATE KEY";
-
-/// PEM tags for other formats
-pub const PEM_PARAMETERS_TAG: &str = "PARAMETERS";
-pub const PEM_SSL_SESSION_PARAMETERS_TAG: &str = "SSL SESSION PARAMETERS";
-
 /// SOAP element names
 pub const SOAP_BODY_END_TAG: &str = "</soap:Body>";
 pub const BODY_END_TAG: &str = "</Body>";
