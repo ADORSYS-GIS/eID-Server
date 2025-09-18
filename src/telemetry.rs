cfc1c21300ec6c70ffca6416d1c82b48f7225416
Hermann Core <hermannoenpause@gmail.com>
2025-09-18 14:22:40 +0100

    refactor: improve formatting of tracing subscriber imports

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,5 +1,10 @@
 use std::sync::OnceLock;
-use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt as _, util::SubscriberInitExt as _};
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt as _,
+    util::SubscriberInitExt as _,
+};
 
 static INIT: OnceLock<()> = OnceLock::new();
 
@@ -7,8 +12,10 @@ pub fn init_tracing() {
     let _ = INIT.get_or_init(|| {
         let env_filter = EnvFilter::try_from_default_env()
             .unwrap_or_else(|_| EnvFilter::new("info,tower_http=info"));
+        let span_event = fmt::layer().with_span_events(FmtSpan::ENTER | FmtSpan::EXIT);
         let _ = tracing_subscriber::registry()
             .with(fmt::layer())
+            .with(span_event)
             .with(env_filter)
             .try_init();
     });
