830e27ffbf4408db7bddae43e6db7aa6d38568e6
Hermann Core <hermannoenpause@gmail.com>
2025-09-29 12:03:28 +0100

    refactor: remove unused serde dependencies from SessionData

--- src/session/data.rs
+++ src/session/data.rs
@@ -1,8 +1,7 @@
 use crate::domain::models::{State, eid::UseIDRequest};
 use bincode::{Decode, Encode};
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Decode, Encode)]
+#[derive(Debug, Decode, Encode)]
 pub struct SessionData {
     pub request_data: UseIDRequest,
     pub psk: Vec<u8>,
