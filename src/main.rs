b77ba7e767ac3165b089c92b3b6cdb65aef52475
Ngha-Boris <borisngha6@gmail.com>
2025-10-02 11:08:24 +0100

    chore: change cron job to rerun daily

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use eid_server::config::Config;
 use eid_server::server::Server;
-use eid_server::setup::setup;
-use eid_server::telemetry;
-
+use eid_server::{setup::setup, telemetry};
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
