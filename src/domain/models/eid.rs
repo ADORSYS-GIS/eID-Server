ecd6fc004b649e76325e7c2f1be5ab31bef20cc5
Hermann Core <hermannoenpause@gmail.com>
2025-10-15 10:20:23 +0100

    refactor: skip serialiazation of empty Operations attributes

--- src/domain/models/eid.rs
+++ src/domain/models/eid.rs
@@ -13,54 +13,71 @@ use validator::Validate;
 pub struct Operations<T: Default> {
     #[serde(rename(serialize = "eid:DocumentType"))]
     #[serde(rename(deserialize = "DocumentType"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub document_type: Option<T>,
     #[serde(rename(serialize = "eid:IssuingState"))]
     #[serde(rename(deserialize = "IssuingState"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub issuing_state: Option<T>,
     #[serde(rename(serialize = "eid:DateOfExpiry"))]
     #[serde(rename(deserialize = "DateOfExpiry"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub date_of_expiry: Option<T>,
     #[serde(rename(serialize = "eid:GivenNames"))]
     #[serde(rename(deserialize = "GivenNames"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub given_names: Option<T>,
     #[serde(rename(serialize = "eid:FamilyNames"))]
     #[serde(rename(deserialize = "FamilyNames"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub family_names: Option<T>,
     #[serde(rename(serialize = "eid:ArtisticName"))]
     #[serde(rename(deserialize = "ArtisticName"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub artistic_name: Option<T>,
     #[serde(rename(serialize = "eid:AcademicTitle"))]
     #[serde(rename(deserialize = "AcademicTitle"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub academic_title: Option<T>,
     #[serde(rename(serialize = "eid:DateOfBirth"))]
     #[serde(rename(deserialize = "DateOfBirth"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub date_of_birth: Option<T>,
     #[serde(rename(serialize = "eid:PlaceOfBirth"))]
     #[serde(rename(deserialize = "PlaceOfBirth"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub place_of_birth: Option<T>,
     #[serde(rename(serialize = "eid:Nationality"))]
     #[serde(rename(deserialize = "Nationality"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub nationality: Option<T>,
     #[serde(rename(serialize = "eid:BirthName"))]
     #[serde(rename(deserialize = "BirthName"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub birth_name: Option<T>,
     #[serde(rename(serialize = "eid:PlaceOfResidence"))]
     #[serde(rename(deserialize = "PlaceOfResidence"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub place_of_residence: Option<T>,
     #[serde(rename(serialize = "eid:CommunityID"))]
     #[serde(rename(deserialize = "CommunityID"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub community_id: Option<T>,
     #[serde(rename(serialize = "eid:ResidencePermitI"))]
     #[serde(rename(deserialize = "ResidencePermitI"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub residence_permit_i: Option<T>,
     #[serde(rename(serialize = "eid:RestrictedID"))]
     #[serde(rename(deserialize = "RestrictedID"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub restricted_id: Option<T>,
     #[serde(rename(serialize = "eid:AgeVerification"))]
     #[serde(rename(deserialize = "AgeVerification"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub age_verification: Option<T>,
     #[serde(rename(serialize = "eid:PlaceVerification"))]
     #[serde(rename(deserialize = "PlaceVerification"), default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub place_verification: Option<T>,
 }
 
